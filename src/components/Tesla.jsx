eed302060d57ff9697190983ef2c326a09a473b5
Steve Silva <steveantoniosilva@gmail.com>
2023-02-07 19:50:24 -0500

    Increase z-index for navbar hamburger

--- src/components/Tesla.jsx
+++ src/components/Tesla.jsx
@@ -1,5 +1,11 @@
 import '../css-components/Tesla.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  ZoomControl,
+} from 'react-leaflet';
 import teslaSvg from '../assets/teslaIcon.svg.png';
 import teslaData from '../../src/TeslaData';
 import { Icon } from 'leaflet';
@@ -16,8 +22,10 @@ function Tesla() {
   return (
     <MapContainer
       center={[38.1230613, -95.9939304]}
+      zoomControl={false}
       zoom={3}
       scrollWheelZoom={false}>
+      <ZoomControl position='topright' />
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
