b21a90a0e4643d98e51077d309d388b1d2a096fc
Steve Silva <steveantoniosilva@gmail.com>
2023-02-08 17:44:50 -0500

    Style Updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,22 @@ export default function Home() {
   return (
     <div className='home'>
       <div className='title'>
-        <div>
-          <h1>Steve</h1>
-        </div>
-        <div>
-          <h1 className='antonio'>Antonio</h1>
-        </div>{' '}
-        <div>
-          <h1>Silva</h1>
+        <div className='title-inner-div'>
+          <div>
+            <h1>Steve</h1>
+          </div>
+          <div>
+            <h1 className='antonio'>Antonio</h1>
+          </div>{' '}
+          <div>
+            <h1>Silva</h1>
+          </div>
         </div>
       </div>
       <div className='sub-title'>
         {' '}
         <p>
-          <span className='react-span'>React |</span>
+          <span className='react-span'>React</span>
           <span className='engineer-span'>Engineer</span>
         </p>
       </div>
