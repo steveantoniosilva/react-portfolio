9671416c9e440d91b2bf7cb2629be1f111545d03
Steve Silva <steveantoniosilva@gmail.com>
2023-02-11 07:54:47 -0500

    ADD EXPERIENCE HISTORY TO RESUME

--- src/components/resume/ResumeWork.jsx
+++ src/components/resume/ResumeWork.jsx
@@ -2,68 +2,136 @@ export default function ResumeExperience() {
   return (
     <div className='resume-experience-page'>
       <div className='experience-item-container'>
-        <h1 className='experience-title'>Experience</h1>
-        <div className='experience-title-and-date'>
+        <h1 className='experience-page-title'>Experience</h1>
+        <div className='experience-content'>
           <div>
-            <h2>EPIC REACT by KENT C. DODDS</h2>
+            <h2>
+              CURRENTLY ENROLLED IN EPIC REACT{' '}
+              <span style={{ textTransform: 'lowercase' }}>by</span> KENT C.
+              DODDS (FULL-TIME)
+            </h2>
           </div>
           <div>
-            <h3>CURRENT</h3>
+            <h3 className='date'>23'</h3>
           </div>
         </div>
-        <h3>REACT DEVELOPER APPRENTICE</h3>
-        <h5>
-          - I'M CURRENTLY PURSUING MY GOAL OF BECOMING A SOFTWARE DEVELOPER
+        <h3 className='experience-content-subtitle'>
+          WORLD CLASS REACT TRAINING COURSE FOR PROFESSIONAL REACT DEVELOPERS
+        </h3>
+        <h5 className='experience-content-h5'>
+          - I LEFT MY SOFTWARE TRAINING ROLE AT A TECH CO. TO FOCUS FULL-TIME ON
+          BECOMING A REACT DEVELOPER
         </h5>
-        <h5>- I'M CURRENTLY WORKING FULL-TIME TO DEVELOP MY REACT SKILLS</h5>
       </div>
       <div className='experience-item-container'>
-        <div className='experience-title-and-date'>
+        <div className='experience-content'>
           <div>
-            <h2>SOFTWARE CO. FOR NON-PROFITS</h2>
+            <h2>SOFTWARE TRAINING & IMPLEMENTATION</h2>
           </div>
           <div>
-            <h3>21'-22'</h3>
+            <h3 className='date'>21'-22'</h3>
           </div>
         </div>
-        <h3>SOFTWARE TRAINING & ONBOARDING</h3>
-        <h5>
+
+        <h3 className='experience-content-subtitle'>
+          SOFTWARE CO. FOR NON-PROFITS
+        </h3>
+
+        <h5 className='experience-content-h5'>
           - HOST LIVE SOFTWARE TRAINING SESSIONS FOR NON PROFITS ACROSS THE U.S.
         </h5>
-        <h5>- SOFTWARE TESTING TO IDENTIFY & DOCUMENT BUGS FOR OUR DEV TEAM</h5>
-        <h5>- DNS & AWS SSL SETUP, DATA IMPORTS & DATA REPORTING</h5>
+        <h5 className='experience-content-h5'>
+          - SOFTWARE TESTING TO IDENTIFY & DOCUMENT BUGS FOR OUR DEV TEAM
+        </h5>
+        <h5 className='experience-content-h5'>
+          - DNS & AWS SSL SETUP, DATA IMPORTS & DATA REPORTING
+        </h5>
       </div>
       <div className='experience-item-container'>
-        <div className='experience-title-and-date'>
+        <div className='experience-content'>
           <div>
-            <h2>3</h2>
+            <h2>CODING BOOTCAMP</h2>
           </div>
           <div>
-            <h3>21'-22'</h3>
+            <h3 className='date'>20'</h3>
           </div>
         </div>
-        <h3>SOFTWARE TRAINING & ONBOARDING</h3>
-        <h5>
-          - HOST LIVE SOFTWARE TRAINING SESSIONS FOR NON PROFITS ACROSS THE U.S.
+        <h3 className='experience-content-subtitle'>TECH TALENT SOUTH</h3>
+        <h5 className='experience-content-h5'>
+          - AFTER BEING LAID OFF DURING COVID & DISCOVERING SOFTWARE
+          DEVELOPMENT, I ENROLLED IN A CODING BOOTCAMP AT TECH TALENT SOUTH
         </h5>
-        <h5>- SOFTWARE TESTING TO IDENTIFY & DOCUMENT BUGS FOR OUR DEV TEAM</h5>
-        <h5>- DNS & AWS SSL SETUP, DATA IMPORTS & DATA REPORTING</h5>
       </div>
       <div className='experience-item-container'>
-        <div className='experience-title-and-date'>
+        <div className='experience-content'>
           <div>
-            <h2>MBA CANDIDATE / GRADUATE RESEARCH ASSISTANT</h2>
+            <h2>LABOR & FARM MANAGER</h2>
           </div>
           <div>
-            <h3>21'-22'</h3>
+            <h3 className='date'>20'</h3>
           </div>
         </div>
-        <h3>SOFTWARE TRAINING & ONBOARDING</h3>
-        <h5>
-          - HOST LIVE SOFTWARE TRAINING SESSIONS FOR NON PROFITS ACROSS THE U.S.
+        <h3 className='experience-content-subtitle'>
+          HIGH-TECH FARMING START-UP
+        </h3>
+        <h5 className='experience-content-h5'>
+          - I LEARNED A TON ABOUT MANAGEMENT WHILE LEADING 40-50 WORKERS ON THE
+          FARM
         </h5>
-        <h5>- SOFTWARE TESTING TO IDENTIFY & DOCUMENT BUGS FOR OUR DEV TEAM</h5>
-        <h5>- DNS & AWS SSL SETUP, DATA IMPORTS & DATA REPORTING</h5>
+        <h5 className='experience-content-h5'>
+          - THE BAD NEWS: I GOT LAID OFF DURING COVID
+        </h5>
+        <h5 className='experience-content-h5'>
+          - THE GOOD NEWS: THIS IS HOW & WHEN I FOUND COMPUTER PROGRAMMING.
+        </h5>
+      </div>
+      <div className='experience-item-container'>
+        <div className='experience-content'>
+          <div>
+            <h2>MBA CANDIDATE & GRADUATE RESEARCH ASSISTANT</h2>
+          </div>
+          <div>
+            <h3>17'-19'</h3>
+          </div>
+        </div>
+        <h3 className='experience-content-subtitle'>
+          COLLEGE OF BUSINESS & ECONOMICS
+        </h3>
+        <h5 className='experience-content-h5'>
+          - I WORKED WITH START-UPS IN OUR ENTREPRENEURSHIP INCUBATOR & I
+          PERFORMED MARKET RESEARCH, ANALYSIS & CONSULTING PROJECTS
+        </h5>
+      </div>
+      <div className='experience-item-container'>
+        <div className='experience-content'>
+          <div>
+            <h2>UNDERGRAD</h2>
+          </div>
+          <div>
+            <h3>14'-17'</h3>
+          </div>
+        </div>
+        <h3 className='experience-content-subtitle'>
+          BACHELORS OF APPLIED SCIENCE, EMPHASIS IN BUSINESS COMMUNICATION
+        </h3>
+        <h5 className='experience-content-h5'>- MAGNA CUM LAUDE</h5>
+      </div>
+      <div className='experience-item-container'>
+        <div className='experience-content'>
+          <div>
+            <h2>SILVA CATTLE COMPANY</h2>
+          </div>
+          <div>
+            <h3>08'-14'</h3>
+          </div>
+        </div>
+        <h3 className='experience-content-subtitle'>FARM & RANCH MANAGER</h3>
+        <h5 className='experience-content-h5'>
+          - MANAGED HARVEST OPERATIONS FOR MULTI-SITE FARM & RANCH
+        </h5>
+        <h5 className='experience-content-h5'>
+          - ANALYZED AGRICULTURAL MARKET RESEARCH & CATTLE ANALYTICS
+        </h5>
       </div>
     </div>
   );
